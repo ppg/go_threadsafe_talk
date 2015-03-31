fb5b59d15ee56c38c82da3e59d8bcf854f0b6fbf
Peter P. Gengler <peter.gengler@sendgrid.com>
2015-03-31 10:05:46 -0600

    Refactor to make the N goroutines more obvious from the test items.

--- benchmark_test.go
+++ benchmark_test.go
@@ -6,71 +6,68 @@ import (
 	"testing"
 )
 
+// Runs f in n go routines, waiting until they all complete
+func benchmarkNGoroutines(n int, f func()) {
+	wg := &sync.WaitGroup{}
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			f()
+		}()
+	}
+	wg.Wait()
+}
+
 func benchmarkFmtPrint(routines int, b *testing.B) {
-	for j := 0; j < b.N; j++ {
-		wg := &sync.WaitGroup{}
-		for i := 0; i < routines; i++ {
-			wg.Add(1)
-			go func() {
-				fmt.Print("")
-				wg.Done()
-			}()
-		}
-		wg.Wait()
+	for i := 0; i < b.N; i++ {
+		benchmarkNGoroutines(routines, func() {
+			fmt.Print("")
+		})
 	}
 }
+
 func BenchmarkFmtPrint1(b *testing.B)    { benchmarkFmtPrint(1, b) }
 func BenchmarkFmtPrint10(b *testing.B)   { benchmarkFmtPrint(10, b) }
 func BenchmarkFmtPrint100(b *testing.B)  { benchmarkFmtPrint(100, b) }
 func BenchmarkFmtPrint1000(b *testing.B) { benchmarkFmtPrint(1000, b) }
 
 func benchmarkMutexFmtPrint(routines int, b *testing.B) {
-	for j := 0; j < b.N; j++ {
-		wg := &sync.WaitGroup{}
-		var mutex sync.Mutex
-		for i := 0; i < routines; i++ {
-			wg.Add(1)
-			go func() {
-				mutex.Lock()
-				fmt.Print("")
-				mutex.Unlock()
-				wg.Done()
-			}()
-		}
-		wg.Wait()
+	var mutex sync.Mutex
+	for i := 0; i < b.N; i++ {
+		benchmarkNGoroutines(routines, func() {
+			mutex.Lock()
+			fmt.Print("")
+			mutex.Unlock()
+		})
 	}
 }
+
 func BenchmarkMutexFmtPrint1(b *testing.B)    { benchmarkMutexFmtPrint(1, b) }
 func BenchmarkMutexFmtPrint10(b *testing.B)   { benchmarkMutexFmtPrint(10, b) }
 func BenchmarkMutexFmtPrint100(b *testing.B)  { benchmarkMutexFmtPrint(100, b) }
 func BenchmarkMutexFmtPrint1000(b *testing.B) { benchmarkMutexFmtPrint(1000, b) }
 
 func benchmarkChannelFmtPrint(routines int, b *testing.B) {
-	for j := 0; j < b.N; j++ {
-		c := make(chan string)
-		q := make(chan struct{})
-		go func() {
-			for {
-				select {
-				case s := <-c:
-					fmt.Print(s)
-				case <-q:
-					return
-				}
+	c := make(chan string)
+	q := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case s := <-c:
+				fmt.Print(s)
+			case <-q:
+				return
 			}
-		}()
-
-		wg := &sync.WaitGroup{}
-		for i := 0; i < routines; i++ {
-			wg.Add(1)
-			go func() {
-				c <- ""
-				wg.Done()
-			}()
 		}
-		wg.Wait()
-		q <- struct{}{}
+	}()
+
+	for i := 0; i < b.N; i++ {
+		benchmarkNGoroutines(routines, func() {
+			c <- ""
+		})
 	}
+	q <- struct{}{}
 }
 
 func BenchmarkChannelFmtPrint1(b *testing.B)    { benchmarkChannelFmtPrint(1, b) }
